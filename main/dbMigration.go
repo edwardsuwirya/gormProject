65ace4faeb366ef790cd8cc14511d0b4a8e26419
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-08-27 15:15:43 +0700

    DB Migration console log info

--- main/dbMigration.go
+++ main/dbMigration.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/edwardsuwirya/gormProject/entity"
 )
 
 func (a app) runMigration() {
 	var migrator = a.db.Migrator()
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("%-30s %30s\n", "DB Migration", "[Failed]")
+		}
+	}()
+	fmt.Printf("%-30s %30s\n", "Checking existing table", "[Done]")
+
 	if migrator.HasTable("m_product") {
-		migrator.DropTable("m_product")
+		if err := migrator.DropTable("m_product"); err != nil {
+			fmt.Printf("%-30s %30s\n", "Dropping table product", "[Failed]")
+		}
 	}
 	if migrator.HasTable("m_category") {
-		migrator.DropTable("m_category")
+		if err := migrator.DropTable("m_category"); err != nil {
+			fmt.Printf("%-30s %30s\n", "Dropping table category", "[Failed]")
+		}
 	}
+	fmt.Printf("%-30s %30s\n", "Creating table structure", "[Done]")
 	if err := a.db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&entity.Category{}, &entity.Product{}); err != nil {
+		fmt.Printf("%-30s %30s\n", "Creating table structure", "[Err]")
 		panic(err)
 	}
 
+	fmt.Printf("%-30s %30s\n", "Insert some sample data", "[Done]")
 	products := []entity.Product{
 		{
 			ProductCode: "001",
@@ -46,7 +61,10 @@ func (a app) runMigration() {
 			},
 		},
 	}
-	a.db.Create(products)
+	if err := a.db.Create(products).Error; err != nil {
+		fmt.Printf("%-30s %30s\n", "Insert some sample data", "[Failed]")
+		panic(err)
+	}
 
 	cat := new(entity.Category)
 	a.db.Where("category_name = ?", "American Food").First(cat)
@@ -56,5 +74,9 @@ func (a app) runMigration() {
 		ProductName: "Cesar Salad",
 		CategoryId:  cat.ID,
 	}
-	a.db.Create(&p)
+	if err := a.db.Create(&p).Error; err != nil {
+		fmt.Printf("%-30s %30s\n", "Insert some sample data", "[Failed]")
+		panic(err)
+	}
+	fmt.Printf("%-30s %30s\n", "DB Migration", "[Done]")
 }
